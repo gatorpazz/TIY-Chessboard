cd6e48a24d3f0c38b0a85e692f2c9645b4754a9e
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-27 03:47:41 -0400

    hashed out a models sheet

--- scripts/models.js
+++ scripts/models.js
@@ -1,1 +1,43 @@
-code
+(function() {
+  var chessGameRevealingModule = (function() {
+    function initBoard() {
+      var board = [
+        ['R','N','B','Q','K','B','N','R'],
+        ['P','P','P','P','P','P','P','P'],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        [' ',' ',' ',' ',' ',' ',' ',' '],
+        ['p','p','p','p','p','p','p','p'],
+        ['r','n','b','q','k','b','n','r'] ];
+      return board;
+    }
+    function move(board, fromX, fromY, toX, toY) {
+
+    }
+    return {
+      board: null;
+    };
+  }
+  var moves = [
+    /*board[4][3] = board[6][3]
+    board[6][3] = ''
+    board[2][5] = board[0][6]
+    board[0][6] = ''
+    board[4][2] = board[6][2]
+    board[6][2] = ''
+    board[2][4] = board[1][4]
+    board[1][4] = ''
+    board[5][6] = board[6][6]
+    board[6][6] = ''
+    board[3][3] = board[1][3]
+    board[1][3] = ''
+    board[6][6] = board[7][5]
+    board[7][5] = ''
+    board[1][4] = board[0][5]
+    board[0][5] = ''
+    board[5][5] = board[7][6]
+    board[7][6] = ''
+    */
+  ]
+})
