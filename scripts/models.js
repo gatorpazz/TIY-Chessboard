da4df70b3d3115c62aa7570c99995b9dc3a5b007
Michael Pascuzzi <gatorpazz@gmail.com>
2015-05-30 10:59:41 -0400

    got tests and shit set up, got pieces what what!?!?!?

--- scripts/models.js
+++ scripts/models.js
@@ -1,25 +1,28 @@
-(function() {
-  var chessGameRevealingModule = (function() {
-    function initBoard() {
-      var board = [
-        ['R','N','B','Q','K','B','N','R'],
-        ['P','P','P','P','P','P','P','P'],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        [' ',' ',' ',' ',' ',' ',' ',' '],
-        ['p','p','p','p','p','p','p','p'],
-        ['r','n','b','q','k','b','n','r'] ];
-      return board;
-    }
-    function move(board, fromX, fromY, toX, toY) {
+(function(window) {
+  window.chess = {
 
-    }
-    return {
-      board: null;
-    };
-  }
-  var moves = [
+  };
+  // var chessGameRevealingModule = (function() {
+  //   function initBoard() {
+  //     var board = [
+  //       ['R','N','B','Q','K','B','N','R'],
+  //       ['P','P','P','P','P','P','P','P'],
+  //       [' ',' ',' ',' ',' ',' ',' ',' '],
+  //       [' ',' ',' ',' ',' ',' ',' ',' '],
+  //       [' ',' ',' ',' ',' ',' ',' ',' '],
+  //       [' ',' ',' ',' ',' ',' ',' ',' '],
+  //       ['p','p','p','p','p','p','p','p'],
+  //       ['r','n','b','q','k','b','n','r'] ];
+  //     return board;
+  //   }
+  //   function move(board, fromX, fromY, toX, toY) {
+  //
+  //   }
+  //   return {
+  //     board: null;
+  //   };
+  // }
+  // var moves = [
     /*board[4][3] = board[6][3]
     board[6][3] = ''
     board[2][5] = board[0][6]
@@ -39,5 +42,5 @@
     board[5][5] = board[7][6]
     board[7][6] = ''
     */
-  ]
-})
+  //]
+})(window);
